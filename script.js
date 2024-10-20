5665b1dec4daf3761768d3d969bfd7c988907595
Shayan A <111425128+ShayanAzfar@users.noreply.github.com>
2024-10-20 16:11:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//  var tl = gsap.timeline();
+
+//  tl
+//  .to("#loader", {
+//     height: 0 ,
+//     duration: 2 ,
+//     ease: Expo.easeInOut
+//  })
+//  .to("#green", {
+//     height: "100%" ,
+//     delay: -2,
+//     duration: 2 ,
+//     ease: Expo.easeInOut
+//  })
+//  .to("#white", {
+//     height: "100%" ,
+//     delay: -1.4,
+//     duration: 2 ,
+//     ease: Expo.easeInOut
+//  })
